32e71e8df1bbb25051c6973f84bd3542577b3eec
JoeyyScott <joey-scott@live.com>
2021-01-20 02:09:57 +0000

    [ADD] Added start quiz button functionality and renamed buttons in index.html to match naming style

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,15 @@
-console.log('JS loaded')
+// Constants for areas of quiz content in index.html
+const buttonStart = document.getElementById('quiz-start');
+const containerWelcome = document.getElementById('welcome-container');
+const containerQuiz = document.getElementById('quiz-container')
+
+//Initiating the quiz on start button being clicked
+buttonStart.addEventListener('click', quizStart);
+
+//The quizStart function will show the questions 
+
+function quizStart() {
+    containerWelcome.classList.add('contentHidden');
+    containerQuiz.classList.remove('contentHidden');
+    questionCounter = 0;
+}
